3e5e4141bd0b4f00b5d7dab8ea0468a7ac05de22
9wan9hyeon <54222988+kimgwang-hyeon@users.noreply.github.com>
2025-04-10 17:40:37 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 90 ms, Memory: 60,416 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,31 +1,24 @@
-def bfs(si,sj):
-    q = []
-    q.append((si,sj))
-    visited[si][sj] = 1
-     
+from collections import deque
+
+
+for tc in range(1, 11):
+    t = int(input())
+    arr = [list(map(int, input())) for _ in range(16)]
+    q = deque()
+    visited = [[0] * 16 for _ in range(16)]
+    q.append((1, 1))
+    visited[1][1] = 1
+    result = True
     while q:
-        i,j = q.pop(0)
-        for dir in range(4):
-            ni = i + di[dir]
-            nj = j + dj[dir]
-            if ni < 0 or ni >= 16 or nj < 0 or nj >= 16:
-                continue
-            if (maze[ni][nj] == 0 or maze[ni][nj] == 3) and visited[ni][nj] == 0:
+        i, j = q.popleft()
+        for di, dj in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < 16 and 0 <= nj < 16 and arr[ni][nj] != 1 and visited[ni][nj] == 0:
                 q.append((ni,nj))
                 visited[ni][nj] = 1
- 
- 
- 
-di = [-1,1,0,0]
-dj = [0,0,-1,1]
-for tc in range(1,11):
-    tc = int(input())
-    maze = [list(map(int,input())) for _ in range(16)]
-    visited = [[0]*16 for _ in range(16)]
-    bfs(1,1)
-    # for v in visited:
-    #     print(v)
-    if visited[13][13] == 1:
-        print(f'#{tc} {1}')
-    else:
-        print(f'#{tc} {0}')
+                if arr[ni][nj] == 3:
+                    result = False
+                    print(f'#{tc} 1')
+
+    if result == True:
+        print(f'#{tc} 0')
